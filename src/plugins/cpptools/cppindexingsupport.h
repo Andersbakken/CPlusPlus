644c5369ea9a0de4880465cbdc4568f76da2e965
Jan Erik Hanssen <jhanssen@gmail.com>
2013-04-12 15:15:10 -0700

    foo

---
+++ src/plugins/cpptools/cppindexingsupport.h
@@ -0,0 +1,134 @@
+/****************************************************************************
+**
+** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of Qt Creator.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+****************************************************************************/
+
+#ifndef CPPTOOLS_CPPINDEXINGSUPPORT_H
+#define CPPTOOLS_CPPINDEXINGSUPPORT_H
+
+#include "cpptools_global.h"
+
+#include <QFuture>
+#include <QStringList>
+
+namespace Find
+{
+enum FindFlag {
+    FindBackward = 0x01,
+    FindCaseSensitively = 0x02,
+    FindWholeWords = 0x04,
+    FindRegularExpression = 0x08,
+    FindPreserveCase = 0x10
+};
+
+Q_DECLARE_FLAGS(FindFlags, FindFlag)
+
+struct CPPTOOLS_EXPORT SearchResultItem
+{
+    SearchResultItem()
+        : textMarkPos(-1),
+          textMarkLength(0),
+          lineNumber(-1),
+          useTextEditorFont(false)
+    {
+    }
+
+    SearchResultItem(const SearchResultItem &other)
+        : path(other.path),
+          text(other.text),
+          textMarkPos(other.textMarkPos),
+          textMarkLength(other.textMarkLength),
+          lineNumber(other.lineNumber),
+          useTextEditorFont(other.useTextEditorFont),
+         userData(other.userData)
+    {
+    }
+
+    QStringList path; // hierarchy to the parent item of this item
+    QString text; // text to show for the item itself
+    int textMarkPos; // 0-based starting position for a mark (-1 for no mark)
+    int textMarkLength; // length of the mark (0 for no mark)
+    int lineNumber; // (0 or -1 for no line number)
+    bool useTextEditorFont;
+    QVariant userData; // user data for identification of the item
+};
+
+}
+
+namespace CppTools {
+
+class CPPTOOLS_EXPORT SymbolSearcher: public QObject
+{
+    Q_OBJECT
+
+public:
+    enum SymbolType {
+        Classes      = 0x1,
+        Functions    = 0x2,
+        Enums        = 0x4,
+        Declarations = 0x8
+    };
+
+    Q_DECLARE_FLAGS(SymbolTypes, SymbolType)
+
+    enum SearchScope {
+        SearchProjectsOnly,
+        SearchGlobal
+    };
+
+    struct Parameters
+    {
+        QString text;
+        Find::FindFlags flags;
+        SymbolTypes types;
+        SearchScope scope;
+    };
+
+
+public:
+    SymbolSearcher(QObject *parent = 0);
+    virtual ~SymbolSearcher() = 0;
+    virtual void runSearch(QFutureInterface<Find::SearchResultItem> &future) = 0;
+};
+
+
+class CPPTOOLS_EXPORT CppIndexingSupport
+{
+public:
+    virtual ~CppIndexingSupport() = 0;
+
+    virtual QFuture<void> refreshSourceFiles(const QStringList &sourceFiles) = 0;
+    virtual SymbolSearcher *createSymbolSearcher(SymbolSearcher::Parameters parameters, QSet<QString> fileNames) = 0;
+};
+
+} // namespace CppTools
+
+Q_DECLARE_METATYPE(CppTools::SymbolSearcher::SearchScope)
+Q_DECLARE_METATYPE(CppTools::SymbolSearcher::Parameters)
+Q_DECLARE_METATYPE(CppTools::SymbolSearcher::SymbolTypes)
+
+#endif // CPPTOOLS_CPPINDEXINGSUPPORT_H
--- src/plugins/cpptools/cpptools.pri
+++ src/plugins/cpptools/cpptools.pri
@@ -1,13 +1,19 @@
 DEFINES += CPPTOOLS_LIBRARY
 
 SOURCES += \
+    $$PWD/builtinindexingsupport.cpp \
     $$PWD/cppfindreferences.cpp \
+    $$PWD/cppindexingsupport.cpp \
     $$PWD/cppmodelmanager.cpp \
+    $$PWD/searchsymbols.cpp \
     $$PWD/ModelManagerInterface.cpp
 
 HEADERS += \
+    $$PWD/builtinindexingsupport.h \
     $$PWD/cppfindreferences.h \
+    $$PWD/cppindexingsupport.h \
     $$PWD/cppmodelmanager.h \
     $$PWD/cpptoolsconstants.h \
     $$PWD/cpptools_global.h \
+    $$PWD/searchsymbols.h \
     $$PWD/ModelManagerInterface.h
